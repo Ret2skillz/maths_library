76b1d13a2a36a64cf585d9b36b303554a4b4a21a
Ret2skillz <mrabolk2@gmail.com>
2026-02-07 16:30:10 +0800

    added stuff in main?

--- main.c
+++ main.c
@@ -4,13 +4,19 @@
 int main(void) {
     matrix_t *a = matrix_create(3, 3);
     matrix_t *b = matrix_create(3, 3);
+    double scalar = 3;
 
     double array1[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
     double array2[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
     matrix_fill_array(a, array1);
     matrix_fill_array(b, array2);
+
+    matrix_print(a);
     
-    matrix_t *c = matrix_add(a, b);
+    matrix_t *c = matrix_add(matrix_add(a, b), b);
 
     matrix_print(c);
+
+    matrix_print(matrix_scalar_mul(c, scalar));
+
 }
